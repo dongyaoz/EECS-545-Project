a346f37239780e4f1ddbff06ea4bb5a0d5956cca
yanbhliu <55248573+yanbhliu@users.noreply.github.com>
2021-12-06 18:37:27 -0500

    add svhn

--- pytorch/mean_teacher/datasets.py
+++ pytorch/mean_teacher/datasets.py
@@ -10,38 +10,37 @@ import torchvision.transforms as transforms
 from . import data
 from .utils import export
 
-
+# add svhn
 @export
-def imagenet():
-    channel_stats = dict(mean=[0.485, 0.456, 0.406],
-                         std=[0.229, 0.224, 0.225])
+def svhn():                
+    channel_stats = dict(mean=[x/255.0 for x in [111.60893667531344, 113.161274663812, 120.5651276685803]],
+                         std=[x/255.0 for x in [50.49768174025085,  51.25898430316379,  50.24421613903954]])
     train_transformation = data.TransformTwice(transforms.Compose([
-        transforms.RandomRotation(10),
-        transforms.RandomResizedCrop(224),
+        data.RandomTranslateWithReflect(4),
         transforms.RandomHorizontalFlip(),
-        transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),
         transforms.ToTensor(),
         transforms.Normalize(**channel_stats)
     ]))
+
     eval_transformation = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize(**channel_stats)
     ])
 
+    data_dir = 'data-local/images/svhn/by-image'
+
     return {
         'train_transformation': train_transformation,
         'eval_transformation': eval_transformation,
-        'datadir': 'data-local/images/ilsvrc2012/',
-        'num_classes': 1000
+        'datadir': data_dir,
+        'num_classes': 10
     }
 
 
 @export
 def cifar10():
-    channel_stats = dict(mean=[0.4914, 0.4822, 0.4465],
-                         std=[0.2470,  0.2435,  0.2616])
+    channel_stats = dict(mean = [0.485,0.456,0.406], 
+                         std = [0.229,0.224,0.225])
     train_transformation = data.TransformTwice(transforms.Compose([
         data.RandomTranslateWithReflect(4),
         transforms.RandomHorizontalFlip(),
