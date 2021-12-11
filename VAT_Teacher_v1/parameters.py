0ed3d9bcecea45455179440bc996713db2d3ceca
dongyaoz <35473593+dongyaoz@users.noreply.github.com>
2021-12-11 02:14:54 -0500

    Update parameters.py

--- VAT_Teacher_v1/parameters.py
+++ VAT_Teacher_v1/parameters.py
@@ -43,8 +43,10 @@ def get_parameters():
                         help='list of image labels (default: based on directory structure)')
 
     parser.add_argument('--arch', '-a', metavar='ARCH', default='convlarge') # kind of redundant , remove it
-    parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
-                        help='number of data loading workers (default: 4)')
+   # parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+    #                     help='number of data loading workers (default: 4)')
+    parser.add_argument('-j', '--workers', default=2, type=int, metavar='N',
+                        help='number of data loading workers (default: 2)')
     parser.add_argument('--epochs', default=200, type=int, metavar='N',
                         help='number of total epochs to run')
     parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
